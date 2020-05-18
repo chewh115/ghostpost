85ea5543ca222ef1f740236c4ab0e6c271c3f583
Chelsea White <chewh115@gmail.com>
2020-05-18 15:58:35 -0400

    Has sorting working for newest/oldest posts

--- boo/forms.py
+++ boo/forms.py
@@ -1,11 +1,22 @@
 from django import forms
 
 class BoastOrRoastForm(forms.Form):
-    BOAST_OR_ROAST = ((True, 'Boast'), (False, 'Roast'))
+    BOAST_OR_ROAST = [(True, 'Boast'), (False, 'Roast')]
+    title = forms.CharField(max_length=30)
+    post = forms.CharField(max_length=280)
     boast_or_roast = forms.ChoiceField(
-        label='Boast or Roast?',
         widget=forms.RadioSelect,
         choices=BOAST_OR_ROAST
     )
-    title = forms.CharField(max_length=30)
-    post = forms.CharField(max_length=280)
+
+
+class SortPosts(forms.Form):
+    SORT_CHOICES = [
+        ('newest', 'Newest'),
+        ('oldest', 'Oldest'),
+        ('liked', 'Most upvotes'),
+        ('disliked', 'Most downvotes')
+    ]
+    sorting_posts = forms.ChoiceField(
+        choices=SORT_CHOICES
+    )
--- boo/views.py
+++ boo/views.py
@@ -1,14 +1,26 @@
 from django.shortcuts import render, reverse, HttpResponseRedirect
 from boo.models import BoastAndRoast
-from boo.forms import BoastOrRoastForm
+from boo.forms import BoastOrRoastForm, SortPosts
 
 # This answer REALLY helped me work through the booleanfield + its use in views:
 # https://stackoverflow.com/questions/29714763/django-check-if-checkbox-is-selected
 
 # Create your views here.
 def index(request):
-    posts = BoastAndRoast.objects.all().order_by('submit_time')
-    return render(request, 'index.html')
+    context = {}
+    context['posts'] = BoastAndRoast.objects.all()
+    context['form'] = SortPosts()
+    if request.method == 'POST':
+        form = SortPosts(request.POST)
+        if form.is_valid():
+            sort_method = form.cleaned_data.get('sorting_posts')
+            if sort_method == 'newest':
+                context['posts'] = BoastAndRoast.objects.all().order_by('-submit_time')
+            elif sort_method == 'oldest':
+                context['posts'] = BoastAndRoast.objects.all().order_by('submit_time')
+            return render(request, 'index.html', context)
+            
+    return render(request, 'index.html', context)
 
 
 def submit(request):
